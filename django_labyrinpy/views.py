3f69aaa4b5b54387bf599ac95a3e25ad0cbc37af
Kiril Vladimirov <kiril@vladimiroff.org>
2013-07-29 11:19:38 +0300

    Remove obsolete views

--- django_labyrinpy/views.py
+++ django_labyrinpy/views.py
@@ -1,45 +1,7 @@
-# Create your views here.
 from django.http import HttpResponse
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404
+
 from models import Message, Report
-# from forms import SendMessageForm
-from django.http import HttpResponseRedirect
-# from django.forms.models import inlineformset_factory
-# from django.views import generic
-
-# ALL VIEW FUNCTIONS MUST RETURN HttpResponse() as it is
-# HttpRequest() we send to them (urls -> funct_from_here -> func does sth)
-
-
-def home(request):
-    pass
-
-
-def search(request):
-    error = False
-    if 'q' in request.GET:
-        q = request.GET['q']
-        if not q:
-            error = True
-        else:
-            messages = Message.objects.filter(destination__icontains=q)
-            return render(request, 'search_result.html',
-                          {'messages': messages, 'query': q})
-    return render(request, 'search_form.html',
-                  {'error': error})
-
-
-# def message(request):
-#     if request.method == 'POST':
-#         form = SendMessageForm(request.POST)
-#         if form.is_valid():
-#             cd = form.cleaned_data
-#             Message.objects.create(**cd)
-#             return HttpResponseRedirect('/message/thanks/')
-#     else:
-#         form = SendMessageForm(
-#             initial={'message_type': 'text'})
-#     return render(request, 'send_message.html', {'form': form})
 
 
 def create_report(request):
